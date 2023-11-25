dbc32bab5b1efaf530582a3e3dc5b1dd723eecb3
noahbcaldwell <noahbcaldwell@gmail.com>
2023-11-25 09:37:25 -0600

    Initial Commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d}");
+Console.WriteLine($"{Environment.NewLine}Press any key to continute...");
+Console.ReadKey(true);
